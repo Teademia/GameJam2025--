7b5114f45b5eb32084f7962e63e12e425def5408
Zirui Zhou <zirui.zhou@foxmail.com>
2025-06-28 11:10:14 +0800

    Delete duplicated files

--- 少年维特之烦恼/AutoLoad/UIManager.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-@onready var GameWinScenePath:String="res://UI/GameWinUI.tscn"
-
-func GameWin()->void:
-	var CurrentScene=get_tree().current_scene
-	CurrentScene.add_child(load(GameWinScenePath).instantiate())
